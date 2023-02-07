a3812919c078f4a12484a495344b0fc962eae836
Aditya Verma <aditya.verma@capgemini.com>
2023-02-07 11:19:28 +0530

    setting up React and writing some react code

---
+++ FirstReact/index.js
@@ -0,0 +1,5 @@
+//ReactDOM.render(<h1>HEllO There</h1>,document.getElementById('root'));
+//ReactDOM.render(<p>Hi My name is Aditya Verma</p>, document.getElementById('root'))
+//Try to write that 1-liner of React code again! This time,
+//see if you can figure out how to render an <ul> with 2+ <li>s inside*/</li>
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>,document.getElementById('root'))
